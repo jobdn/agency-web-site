c54d57b4d46514d44e5c1e52b24958ccf4202757
Danila <peace_are_off@bk.ru>
2022-06-22 09:44:24 +0300

    feat: add showcase

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { Header } from "./components/Header";
+import { Showcase } from "./components/Showcase";
 
 const StyledApp = styled.div`
   min-height: 100vh;
@@ -12,6 +13,7 @@ const App: React.FC = () => {
   return (
     <StyledApp>
       <Header />
+      <Showcase />
     </StyledApp>
   );
 };
---
+++ src/components/Showcase/Showcase.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Showcase.scss";
+
+export const Showcase: React.FC = () => {
+  return (
+    <div className="showcase">
+      <div className="showcase__title">Portfolio</div>
+      <div className="showcase__subtitle">
+        Agency provides a full service range including technical skills, design,
+        business understanding.
+      </div>
+    </div>
+  );
+};
